1a0f1d98942fc3310dade6113df4ded5f80e38e2
chadunnett <chadunnett@gmail.com>
2022-10-27 17:05:17 -0500

    commit

--- 02-Challenge/Develop/index.js
+++ 02-Challenge/Develop/index.js
@@ -11,3 +11,95 @@ function init() {}
 
 // Function call to initialize app
 init();
+const inquirer = require('inquirer');
+const fs = require('fs');
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is the title?',
+      name: 'title',
+    },
+    {
+      type: 'input',
+      message: 'What is the description?',
+      name: 'description',
+    },
+    {
+      type: 'input',
+      message: 'What are the installation steps?',
+      name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'What is the usage?',
+        name: 'usage',
+      },
+      {
+        type: 'input',
+        message: 'What license to use?',
+        name: 'license',
+      },
+      {
+        type: 'input',
+        message: 'What are the contributions?',
+        name: 'contributions',
+      },
+      {
+          type: 'input',
+          message: 'What are the tests?',
+          name: 'tests',
+        },
+        {
+          type: 'input',
+          message: 'What are the questions?',
+          name: 'questions',
+        },
+  ])
+.then((answers) => {
+    const markdown = `# <${answers.title}>
+
+    ## Description
+    
+    ${answers.description}
+    
+    ## Table of Contents
+    
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [Credits](#credits)
+    - [License](#license)
+    - [Tests](#tests)
+    - [Questions](#questions)
+    
+    ## Installation
+    
+    ${answers.installation}
+    
+    ## Usage
+    
+    ${answers.usage}
+    
+    ## Credits
+    
+    ${answers.contributions}
+    
+    ## License
+    
+    ${answers.license}
+    
+    ## How to Contribute
+    
+    If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+    
+    ## Tests
+    
+    ${answers.tests}
+    
+    ## Questions
+    
+    ${answers.questions}`
+    fs.writeFile('README,md',markdown, (err) =>
+  err ? console.error(err) : console.log('Success!')
+);
+});
--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# READMEGenerator
+# <${title}>
+
+## Description
+
+${description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+${installation}
+
+## Usage
+
+${usage}
+
+To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+
+    ```md
+    ![alt text](assets/images/screenshot.png)
+    ```
+
+## Credits
+
+${answers.contributions}
+
+## License
+
+${license}
+
+## How to Contribute
+
+If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+
+## Tests
+
+${tests}
+
+## Questions
+
+${questions}
